1d880f79f51a509ea9980dbb0c9f4d20afd07c98
Ben Scobie <admin@benscobie.com>
2013-02-11 16:26:39 +0000

    Removed CSS declaration Removed a few bits of unneeded code A more improved README

--- README.md
+++ README.md
@@ -1,41 +1,67 @@
-# Last.fm Status - No jQuery
+# Last.fm Status
 
-Last.fm Status uses the Last.fm API to show your currently playing track.
+Last.fm Status utilises the Last.fm API to show your currently playing track on a website using JavaScript.
 
 ## Install
 
-Download the .js file or use the lastest from here and include it on the page you want to display your last or currently playing track, such as:
+Download the last_fm.js file and include it on the page(s) you want to display your currently playing track. The following code is usually placed inside the head tags:
 ```html
-<script type="text/javascript" src="https://raw.github.com/benscobie/lastfm-status/nojquery/last_fm.js"></script>
+<script type="text/javascript" src="last_fm.js"></script>
+<script>
+	window.onload = function() {
+		LastFMStatus.init({
+			username: "desired user name"
+		});
+	};
+</script>
 ```
 
-Add the following HTML to your page. If the div block is not added then the track info will appear at the end of the page.
+Preferably you would specify your own Last.fm API Key (aquired at www.last.fm/api) like so:
+```js
+<script>
+	window.onload = function() {
+		LastFMStatus.init({
+			username: "desired user name",
+			apikey: "key"
+		});
+	};
+</script>
+```
+
+Add the following HTML to your page where you want it to be displayed. If this block is not added then the track information will appear at the bottom of the page.
 ```html
 <div id="lfm">Loading</div>
 ```
 
-```js
-window.onload = function() {
-  LastFMStatus.init({
-    username: "desired user name"
-  });
-};
-```
+Customize the CSS to your liking and add it to your stylesheet:
+```css
+#lfm {
+	font-family: Georgia;
+	color: white;
+	text-align: center;
+	padding: 0 20px;
+	line-height: 60px;
+	background: #DC5248;
+	max-width: 600px;
+	min-width: 200px; 
+	border-top: 10px solid #AA2B1D;
+	border-bottom: 10px solid #AA2B1D;
+	font-size: 15px;
+}
 
-Preferably you would specify your own Last.fm API Key by using the following instead:
-```js
-window.onload = function() {
-  LastFMStatus.init({
-    username: "desired user name",
-	apikey: "key"
-  });
-};
-```
+strong {
+	font-style: italic;
+	text-shadow: 1px -2px 4px #962215;
+}
 
+a, a:visited {
+	color:#FFCFCF;
+}
+```
 
 ## Example:
 
-See demo here: http://sfate.github.com/lastfm-status/
+See a demo by Sfate here: http://sfate.github.com/lastfm-status/
 
 ## Contribute
 
--- last_fm.js
+++ last_fm.js
@@ -1,89 +1,77 @@
 var LastFMStatus = {
-  defaultApiKey    : '8268a36df9e8ca5f3bf2dac06f83ef93',
-  updateIntervalID : null,
-  updateDelay      : 60000,
-  apikey           : null,
-  username         : null,
-  trackInfo        : null,
+	defaultApiKey    : '8268a36df9e8ca5f3bf2dac06f83ef93',
+	updateDelay      : 60000,
+	apikey           : null,
+	username         : null,
+	trackInfo        : null,
 
-  init: function(options) {
-    options = options || {};
-    this.apikey = (options.apikey ? options.apikey:this.defaultApiKey);
-    if (options.username) {
-      this.username = options.username;
-    } else {
-      throw 'RuntimeError: No username was specified!';
-    }
-    this.insertCSS();
-    this.fetch();
-  },
+	init: function(options) {
+		options = options || {};
+		this.apikey = (options.apikey ? options.apikey:this.defaultApiKey);
+		if (options.username) {
+			this.username = options.username;
+		} else {
+			throw 'RuntimeError: No username was specified!';
+		}
+		this.fetch();
+	},
 
-  url: function(callback) {
-    return 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+this.username+'&api_key='+this.apikey+'&limit=2&format=json&callback='+callback;
-  },
+	url: function(callback) {
+		return 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+this.username+'&api_key='+this.apikey+'&limit=2&format=json&callback='+callback;
+	},
 
-  insertCSS: function() {
-    var style       = document.createElement('style');
-    style.innerHTML = "#lfm{font-family:Georgia; color:white; text-align:center; padding:0 20px; line-height:60px; background: #DC5248;"
-                      +"max-width:600px; min-width:200px;  border-top:10px solid #AA2B1D; border-bottom:10px solid #AA2B1D; font-size:15px;}"
-                      +"strong{font-style:italic; text-shadow:1px -2px 4px #962215;} a, a:visited{color:#FFCFCF;}";
-    document.body.appendChild(style);
-  },
+	fetch: function() {
+		var oldScript = document.getElementById('lfm_state_json');
+		if (oldScript) {
+			document.body.removeChild(oldScript);
+		}
+		var script = document.createElement('script');
+		script.src = this.url('LastFMStatus.updateInfo');
+		script.id  = 'lfm_state_json'
+		document.body.appendChild(script);
+	},
 
-  fetch: function() {
-    var oldScript = document.getElementById('lfm_state_json');
-    if (oldScript) {
-      document.body.removeChild(oldScript);
-    }
-    var script = document.createElement('script');
-    script.src = this.url('LastFMStatus.updateInfo');
-    script.id  = 'lfm_state_json'
-    document.body.appendChild(script);
-  },
+	updateInfo: function(data) {
+		if (data.error) {
+			this.trackInfo = data;
+		} else {
+			var track = data.recenttracks.track[0];
+			var trackInfo = {
+				song    : track.name,
+				artist  : track.artist["#text"],
+				playing : (track["@attr"] ? true : false)
+			};
+			this.trackInfo = (this.trackInfo || {});
 
-  updateInfo: function(data) {
-    if (data.error) {
-      this.trackInfo = data;
-    } else {
-      var track = data.recenttracks.track[0];
-      var trackInfo = {
-        song    : track.name,
-        artist  : track.artist["#text"],
-        album   : track.album["#text"],
-        image   : track.image[1]["#text"],
-        playing : (track["@attr"] ? true : false)
-      };
-      this.trackInfo = (this.trackInfo || {});
+			if (this.songChanged(trackInfo)) {
+				this.trackInfo = trackInfo;
+			}
+		}
+		this.updateView();
+		setTimeout(function() {LastFMStatus.fetch()}, this.updateDelay);
+	},
 
-      if (this.songChanged(trackInfo)) {
-        this.trackInfo = trackInfo;
-      }
-    }
-    this.updateView();
-    setTimeout(function() {LastFMStatus.fetch()}, this.updateDelay);
-  },
+	songChanged: function(newInfo) {
+		return this.trackInfo.song != newInfo.song || this.trackInfo.playing != newInfo.playing;
+	},
 
-  songChanged: function(newInfo) {
-    return this.trackInfo.song != newInfo.song || this.trackInfo.playing != newInfo.playing;
-  },
-
-  updateView: function() {
-    var status, message,
-    userlink = ' ( <a target="__blank" href="http://www.last.fm/user/' + this.username + '">last.fm</a> )';
-    var statusBox = document.getElementById('lfm');
-    if (!statusBox) {
-      var view = document.createElement('div');
-      view.id  = "lfm";
-      document.body.appendChild(view);
-      statusBox = document.getElementById('lfm');
-    }
-    if (this.trackInfo.error) {
-      status   = "Error: ";
-      message  = '<strong>'+this.trackInfo.message+'</strong>';
-    } else {
-      status = this.trackInfo.playing ? 'Now Playing: ' : 'Last Played: ';
-      message  = '<strong>'+this.trackInfo.song+'</strong> by <strong>'+this.trackInfo.artist+'</strong>';
-    }
-    statusBox.innerHTML = status + message + userlink;
-  }
+	updateView: function() {
+		var status, message,
+		userlink = ' ( <a target="__blank" href="http://www.last.fm/user/' + this.username + '">last.fm</a> )';
+		var statusBox = document.getElementById('lfm');
+		if (!statusBox) {
+			var view = document.createElement('div');
+			view.id  = "lfm";
+			document.body.appendChild(view);
+			statusBox = document.getElementById('lfm');
+		}
+		if (this.trackInfo.error) {
+			status   = "Error: ";
+			message  = '<strong>'+this.trackInfo.message+'</strong>';
+		} else {
+			status = this.trackInfo.playing ? 'Now Playing: ' : 'Last Played: ';
+			message  = '<strong>'+this.trackInfo.song+'</strong> by <strong>'+this.trackInfo.artist+'</strong>';
+		}
+		statusBox.innerHTML = status + message + userlink;
+	}
 };
